f0b120b44b1904d5f587924c45520b5988d0b338
dayanariel <38135637+dayanariel@users.noreply.github.com>
2019-11-09 11:23:43 -0600

    se agrega la activity maps
    en esta actividad contendra la vista del mapa

---
+++ TranslateApp/app/src/main/java/com/argonautas/translate_app/maps.java
@@ -0,0 +1,14 @@
+package com.argonautas.translate_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class maps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
